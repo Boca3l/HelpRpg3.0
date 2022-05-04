8e9ad15b2606381682c94c3c8653ccce5ea11dc5
Pedro Henrique <preisteixeira@gmail.com>
2022-05-04 15:45:21 -0400

    Add head
    Doctype, lang e charset

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
 <link rel="stylesheet" media="screen" href="css/index.css" />
+<meta charset="utf-8">
 
 <script src="js/index.js"></script>
 
  <script src="js/index.js"></script>
 
 <?php
+header('Content-Type: text/html; charset=utf-8');
+
 if(file_exists('init.php')):
 	require 'init.php';
 	require 'class/teste_personagem.class.php';
